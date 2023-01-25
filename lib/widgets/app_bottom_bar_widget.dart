b06964d955af52aa6a0a3bccf899f6d7a37547fa
thyagosouza <thyagosouza@gmail.com>
2023-01-25 15:20:48 -0300

    BottomBar fix

--- lib/widgets/app_bottom_bar_widget.dart
+++ lib/widgets/app_bottom_bar_widget.dart
@@ -12,71 +12,47 @@ class _AppBottomBarWidgetState extends State<AppBottomBarWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: Offset.zero,
-                )
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(
-                barItems.length,
-                (index) {
-                  AppBottomBarItem currentBarItem = barItems[index];
+        margin: EdgeInsets.only(top: 20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: Offset.zero)
+        ]),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(barItems.length, (index) {
+              AppBottomBarItem currentBarItem = barItems[index];
 
-                  Widget barItemWidget;
+              Widget barItemWidget;
 
-                  currentBarItem.isSelected
-                      ? barItemWidget = Container(
-                          padding: EdgeInsets.only(
-                              left: 15, top: 5, bottom: 5, right: 15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: AppColors.mainColor),
-                          child: Row(children: [
-                            Icon(
-                              currentBarItem.icon,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              currentBarItem.label,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ]),
-                        )
-                      : barItemWidget = IconButton(
-                          onPressed: () {
-                            setState(() {
-                              barItems.forEach((AppBottomBarItem item) {
-                                item.isSelected = item == currentBarItem;
-                              });
-                            });
-                          },
-                          icon: Icon(
-                            currentBarItem.icon,
-                            color: Colors.grey,
-                          ),
-                        );
-                  return barItemWidget;
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+              if (currentBarItem.isSelected) {
+                barItemWidget = Container(
+                    padding:
+                        EdgeInsets.only(left: 15, top: 5, bottom: 5, right: 15),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: AppColors.mainColor),
+                    child: Row(children: [
+                      Icon(currentBarItem.icon, color: Colors.white),
+                      SizedBox(width: 5),
+                      Text(currentBarItem.label,
+                          style: TextStyle(color: Colors.white))
+                    ]));
+              } else {
+                barItemWidget = IconButton(
+                    icon: Icon(currentBarItem.icon, color: Colors.grey),
+                    onPressed: () {
+                      setState(() {
+                        barItems.forEach((AppBottomBarItem item) {
+                          item.isSelected = item == currentBarItem;
+                        });
+                      });
+                    });
+              }
+
+              return barItemWidget;
+            })));
   }
 }
--- lib/widgets/app_details_bottom_actions_widget.dart
+++ lib/widgets/app_details_bottom_actions_widget.dart
@@ -5,44 +5,47 @@ import '../utils/app_colors.dart';
 class DetailsBottomActionsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-      child: Row(
-        children: [
-          Expanded(
-            child: Material(
-              borderRadius: BorderRadius.circular(15),
-              color: AppColors.mainColor,
-              child: InkWell(
-                highlightColor: Colors.white.withOpacity(0.2),
-                splashColor: Colors.white.withOpacity(0.2),
-                onTap: () {},
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    'Book Now',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
+    return Container(
+      width: double.maxFinite,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Material(
+                borderRadius: BorderRadius.circular(15),
+                color: AppColors.mainColor,
+                child: InkWell(
+                  highlightColor: Colors.white.withOpacity(0.2),
+                  splashColor: Colors.white.withOpacity(0.2),
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      'Book Now',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.white,
-              border: Border.all(color: AppColors.mainColor, width: 2),
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+                border: Border.all(color: AppColors.mainColor, width: 2),
+              ),
+              child: Icon(
+                Icons.turned_in_not,
+                color: AppColors.mainColor,
+                size: 25,
+              ),
             ),
-            child: Icon(
-              Icons.turned_in_not,
-              color: AppColors.mainColor,
-              size: 25,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
